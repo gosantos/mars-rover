6855f60cb229e6f234ebaa34da3a6edf62682d4c
Guilherme <guilhermeoliveira564@gmail.com>
2020-05-23 19:08:26 +0200

    Implement validate-position function

--- src/mars_rover/core.clj
+++ src/mars_rover/core.clj
@@ -4,14 +4,27 @@
 
 (defrecord Position [x y compass])
 
-(defn move [position]
-  (case (:compass position)
-    "N" (Position. (:x position) (+ (:y position) 1) (:compass position))
-    "S" (Position. (:x position) (- (:y position) 1) (:compass position))
-    "E" (Position. (+ (:x position) 1) (:y position) (:compass position))
-    "W" (Position. (- (:x position) 1) (:y position) (:compass position))
-    (format "Error. I dont know what is %s." (:compass position))))
 
+(defn validate-position [position]
+  (let [valid (and
+                (<= (:x position) (:x plateau))
+                (<= (:y position) (:y plateau))
+                (>= (:x position) 0)
+                (>= (:y position) 0)
+                )]
+    (if valid
+      position
+      (throw (RuntimeException. "You have reach the end of plateau.")))))
+
+
+(defn move [position]
+  (let [new-position (case (:compass position)
+                       "N" (Position. (:x position) (+ (:y position) 1) (:compass position))
+                       "S" (Position. (:x position) (- (:y position) 1) (:compass position))
+                       "E" (Position. (+ (:x position) 1) (:y position) (:compass position))
+                       "W" (Position. (- (:x position) 1) (:y position) (:compass position))
+                       (throw (RuntimeException. (format "Error. I dont know what is %s." (:compass position)))))]
+    (validate-position new-position)))
 
 (def compass-after-rotation
   {:LN "W" :LS "E" :LE "N" :LW "S"
@@ -19,7 +32,4 @@
 
 (defn rotate [direction position]
   (let [new-compass (get compass-after-rotation (keyword (str direction (:compass position))))]
-    (Position.
-      (:x position)
-      (:y position)
-      new-compass)))
+    (Position. (:x position) (:y position) new-compass)))
--- test/mars_rover/core_test.clj
+++ test/mars_rover/core_test.clj
@@ -4,6 +4,13 @@
   (:import (mars_rover.core Position)))
 
 
+(deftest validate-new-position-test
+  (testing "should throw an exception when the movement is invalid"
+    (is (thrown? RuntimeException (validate-position (Position. 5 6 "N"))))
+    (is (thrown? RuntimeException (validate-position (Position. 5 -1 "S"))))
+    (is (thrown? RuntimeException (validate-position (Position. 6 -1 "E"))))
+    (is (thrown? RuntimeException (validate-position (Position. -2 0 "W"))))))
+
 (deftest move-test
   (testing "it should move the rover to north"
     (is (= (Position. 1 3 "N") (move (Position. 1 2 "N")))))
@@ -14,9 +21,7 @@
   (testing "it should move the rover to west"
     (is (= (Position. 0 2 "W") (move (Position. 1 2 "W")))))
   (testing "it should move the rover to bar"
-    (is (= "Error. I dont know what is bar." (move (Position. 1 2 "bar")))))
-  )
-
+    (is (thrown? RuntimeException (move (Position. 1 2 "bar"))))))
 
 (deftest rotate-test
   (testing "it should rotate the rover to north"
@@ -26,5 +31,4 @@
   (testing "it should rotate the rover to east"
     (is (= (Position. 1 2 "S") (rotate "L" (Position. 1 2 "W")))))
   (testing "it should rotate the rover to west"
-    (is (= (Position. 1 2 "S") (rotate "R" (Position. 1 2 "E")))))
-  )
+    (is (= (Position. 1 2 "S") (rotate "R" (Position. 1 2 "E"))))))
