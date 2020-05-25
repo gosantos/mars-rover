129e41fc26b3bf6676537bcfbdbc48b826f3a455
Guilherme <guilhermeoliveira564@gmail.com>
2020-05-25 17:18:24 +0200

    Refactor almost all functions to take plateau as a paramter

--- src/mars_rover/core.clj
+++ src/mars_rover/core.clj
@@ -4,24 +4,23 @@
 
 
 ;; domain
-(def plateau {:x 5 :y 5})
 
 (defrecord Position [x y compass])
 (defrecord Plateau [x y])
 
-(defn validate-position [position]
+(defn validate-position [position, plateau]
   (let [in-plateau (and (<= 0 (:x position) (:x plateau)) (<= 0 (:y position) (:y plateau)))]
     (if in-plateau
       position
       (throw (RuntimeException. "You have reach the end of plateau.")))))
 
-(defn move [position]
+(defn move [position plateau]
   (let [new-position (case (:compass position)
                        "N" (Position. (:x position) (+ (:y position) 1) (:compass position))
                        "S" (Position. (:x position) (- (:y position) 1) (:compass position))
                        "E" (Position. (+ (:x position) 1) (:y position) (:compass position))
                        "W" (Position. (- (:x position) 1) (:y position) (:compass position)))]
-    (validate-position new-position)))
+    (validate-position new-position plateau)))
 
 (defn rotate-left [position]
   (let [new-compass ((keyword (:compass position)) {:N "W" :S "E" :E "N" :W "S"})]
@@ -31,15 +30,15 @@
   (let [new-compass ((keyword (:compass position)) {:N "E" :S "W" :E "S" :W "N"})]
     (Position. (:x position) (:y position) new-compass)))
 
-(defn move-or-rotate [position command]
+(defn move-or-rotate [position command plateau]
   (case command
-    "M" (move position)
+    "M" (move position plateau)
     "L" (rotate-left position)
     "R" (rotate-right position)
     (throw (RuntimeException. (format "Command %s is invalid." command)))))
 
-(defn rover-service [position commands]
-  (reduce #(move-or-rotate %1 %2) position commands))
+(defn rover-service [position commands plateau]
+  (reduce #(move-or-rotate %1 %2 plateau) position commands))
 
 ; converters
 (defn plateau-converter [args]
--- test/mars_rover/core_test.clj
+++ test/mars_rover/core_test.clj
@@ -5,22 +5,25 @@
   (:import (mars_rover.core Position Plateau)))
 
 
+
+(def plateau (Plateau. 5 5))
+
 (deftest validate-new-position-test
   (testing "should throw an exception when the movement is invalid"
-    (is (thrown? RuntimeException (validate-position (Position. 5 6 "N"))))
-    (is (thrown? RuntimeException (validate-position (Position. 5 -1 "S"))))
-    (is (thrown? RuntimeException (validate-position (Position. 6 -1 "E"))))
-    (is (thrown? RuntimeException (validate-position (Position. -2 0 "W"))))))
+    (is (thrown? RuntimeException (validate-position (Position. 5 6 "N") plateau)))
+    (is (thrown? RuntimeException (validate-position (Position. 5 -1 "S") plateau)))
+    (is (thrown? RuntimeException (validate-position (Position. 6 -1 "E") plateau)))
+    (is (thrown? RuntimeException (validate-position (Position. -2 0 "W") plateau)))))
 
 (deftest move-test
   (testing "it should move the rover to north"
-    (is (= (Position. 1 3 "N") (move (Position. 1 2 "N")))))
+    (is (= (Position. 1 3 "N") (move (Position. 1 2 "N") plateau))))
   (testing "it should move the rover to south"
-    (is (= (Position. 1 1 "S") (move (Position. 1 2 "S")))))
+    (is (= (Position. 1 1 "S") (move (Position. 1 2 "S") plateau))))
   (testing "it should move the rover to east"
-    (is (= (Position. 2 2 "E") (move (Position. 1 2 "E")))))
+    (is (= (Position. 2 2 "E") (move (Position. 1 2 "E") plateau))))
   (testing "it should move the rover to west"
-    (is (= (Position. 0 2 "W") (move (Position. 1 2 "W"))))))
+    (is (= (Position. 0 2 "W") (move (Position. 1 2 "W") plateau)))))
 
 (deftest rotate-test
   (testing "it should rotate the rover to north"
@@ -34,19 +37,19 @@
 
 (deftest move-or-rotate-test
   (testing "it should move"
-    (is (= (Position. 1 3 "N") (move-or-rotate (Position. 1 2 "N") "M"))))
+    (is (= (Position. 1 3 "N") (move-or-rotate (Position. 1 2 "N") "M" plateau))))
   (testing "it should rotate left"
-    (is (= (Position. 1 2 "W") (move-or-rotate (Position. 1 2 "N") "L"))))
+    (is (= (Position. 1 2 "W") (move-or-rotate (Position. 1 2 "N") "L" plateau))))
   (testing "it should rotate right"
-    (is (= (Position. 1 2 "N") (move-or-rotate (Position. 1 2 "W") "R"))))
+    (is (= (Position. 1 2 "N") (move-or-rotate (Position. 1 2 "W") "R" plateau))))
   (testing "it should thrown an exception when the movement is invalid"
-    (is (thrown? RuntimeException (move-or-rotate (Position. 1 2 "E") "F")))))
+    (is (thrown? RuntimeException (move-or-rotate (Position. 1 2 "E") "F" plateau)))))
 
 (deftest rover-run-service-test
   (testing "given a list of commands and a initial position it should move the rover the correct position"
-    (is (= (Position. 1 3 "N") (rover-service (Position. 1 2 "N") (list "L" "M" "L" "M" "L" "M" "L" "M" "M")))))
+    (is (= (Position. 1 3 "N") (rover-service (Position. 1 2 "N") (list "L" "M" "L" "M" "L" "M" "L" "M" "M") plateau))))
   (testing "given a list of commands and a initial position it should move the rover the correct position 2"
-    (is (= (Position. 5 1 "E") (rover-service (Position. 3 3 "E") (list "M" "M" "R" "M" "M" "R" "M" "R" "R" "M"))))))
+    (is (= (Position. 5 1 "E") (rover-service (Position. 3 3 "E") (list "M" "M" "R" "M" "M" "R" "M" "R" "R" "M") plateau)))))
 
 (deftest plateau-converter-test
   (testing "should convert to a plateau"
